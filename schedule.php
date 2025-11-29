20b61d7f901417218d87c27358b7cdbaa98258fc
amenta13 <andrewgsoccer2004@gmail.com>
2025-11-29 00:26:10 -0500

    Prepare addition for adding weekly game data

--- schedule.php
+++ schedule.php
@@ -1,27 +1,30 @@
+<?php
+
+require_once 'api/read.php';
+
+?>
+
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="UTF-8">
-    <title>Soccer Schedule</title>
-    <link rel="stylesheet" href="css/style_sheet.css">
-  </head>
-  <body>
 
-    <?php require_once "web_elements/navbar.php" ?>
+<head>
+  <meta charset="UTF-8">
+  <title>Soccer Schedule</title>
+  <link rel="stylesheet" href="css/style_sheet.css">
+</head>
 
-    <main>
-      <h1>Upcoming Matches</h1>
-      <div id="matchList">
-      <!-- Match cards will be shown here -->
-    </div>
-    <script src="js/schedule.js"></script>
-    </main>    
+<body>
 
-    
+  <?php require_once "web_elements/navbar.php" ?>
 
+  <main>
+    <h1>Upcoming Matches</h1>
+  </main>
 
+  <!-- TODO: Add set of games for the game week and list them -->
 
-    <?php require_once "web_elements/footer.php" ?>
+  <?php require_once "web_elements/footer.php" ?>
 
-  </body>
+</body>
+
 </html>
