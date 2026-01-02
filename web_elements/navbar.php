8c9ac5446b6e8ed5d787cd26876d784f0557ea91
amenta13 <andrewgsoccer2004@gmail.com>
2026-01-02 16:23:53 -0500

    Reordered leauges by popularity, then competitions

--- web_elements/navbar.php
+++ web_elements/navbar.php
@@ -11,15 +11,15 @@
           <li><a href="leagues.php?league=PL">Premier League <img src="web_elements/england_flag.png"></a></li>
           <li><a href="leagues.php?league=ELC">English Championship <img src="web_elements/england_flag.png"></a></li>
           <li><a href="leagues.php?league=PD">La Liga <img src="web_elements/spain_flag.png"></a></li>
-          <li><a href="leagues.php?league=PPL">Primeira Liga <img src="web_elements/portugal_flag.png"></a></li>
           <li><a href="leagues.php?league=BL1">Bundesliga <img src="web_elements/germany_flag.png"></a></li>
           <li><a href="leagues.php?league=SA">Serie A <img src="web_elements/italy_flag.png"></a></li>
           <li><a href="leagues.php?league=FL1">Ligue 1 <img src="web_elements/france_flag.png"></a></li>
+          <li><a href="leagues.php?league=BSA">Campeonato Brasiliero Série A <img src="web_elements/brazil_flag.png"></a></li>
+          <li><a href="leagues.php?league=DED">Eredivisie <img src="web_elements/netherlands_flag.png"></a></li>
+          <li><a href="leagues.php?league=PPL">Primeira Liga <img src="web_elements/portugal_flag.png"></a></li>
           <li><a href="leagues.php?league=CL">UEFA Champions League <img src="web_elements/eu_flag.png"></a></li>
           <li><a href="leagues.php?league=EC">European Championship <img src="web_elements/eu_flag.png"></a></li>
           <li><a href="leagues.php?league=WC">FIFA World Cup <img src="web_elements/earth_flag.png"></a></li>
-          <li><a href="leagues.php?league=DED">Eredivisie <img src="web_elements/netherlands_flag.png"></a></li>
-          <li><a href="leagues.php?league=BSA">Campeonato Brasiliero Série A <img src="web_elements/brazil_flag.png"></a></li>
         </ul>
     </li>
     <li><a href="schedule.php">Schedule</a></li>
