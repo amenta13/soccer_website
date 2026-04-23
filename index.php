5cdba22e4f677c752df92efa3c6bf0b14a043d8a
amenta13 <andrewgsoccer2004@gmail.com>
2026-04-23 11:36:23 -0400

    Updated home page with links to each league

--- index.php
+++ index.php
@@ -15,6 +15,21 @@
     <h1>Welcome to the ultimate soccer guide!</h1>
   </main>
 
+  <div class="league-grid">
+    <div><a href="leagues.php?league=WC"><img src="web_elements/logos/2026_FIFA_World_Cup_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=EC"><img src="web_elements/logos/UEFA_Euro_2024_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=CL"><img src="web_elements/logos/UEFA_Champions_League_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=PL"><img src="web_elements/logos/Premier_League_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=ELC"><img src="web_elements/logos/EFL_Championship_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=PD"><img src="web_elements/logos/La_Liga_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=BL1"><img src="web_elements/logos/Bundesliga_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=SA"><img src="web_elements/logos/Serie_A_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=FL1"><img src="web_elements/logos/Ligue_1_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=BSA"><img src="web_elements/logos/Campeonato_Brasileiro_Série_A_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=DED"><img src="web_elements/logos/Eredivisie_logo.png" class="league-logo"></a></div>
+    <div><a href="leagues.php?league=PPL"><img src="web_elements/logos/Primeira_Liga_logo.png" class="league-logo"></a></div>
+  </div>
+
   <?php require_once "web_elements/footer.php" ?>
 
 </body>
