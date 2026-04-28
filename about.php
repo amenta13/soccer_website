45e0135ea5a37b83082b1414e314001ee6403239
amenta13 <andrewgsoccer2004@gmail.com>
2026-04-28 10:50:29 -0400

    Added content to the About page

--- about.php
+++ about.php
@@ -11,8 +11,22 @@
 
     <main>
       <h1>About</h1>
-    </main>    
+    </main>
 
+    <div class="textbox">
+      The purpose of this website is to be able to quickly find upcoming soccer games across multiple leagues and competitions.
+      Currently only 12 leagues and competitions are available, however future additions to the site may include additional
+      leagues and competitions, club and player stats, and viewing options.
+    </div>
+
+    <div class="textbox">
+      The API used for the soccer data found here is from football-data.org.
+      <BR>
+      Check it out yourself <a href="https://www.football-data.org/coverage">here</a>!
+    </div>
+
+    <img src="web_elements/soccer_ball_tv.jpg" class="center-img">
+    
     <?php require_once "web_elements/footer.php" ?>
 
   </body>
